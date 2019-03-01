885e480a486aca88ec5be8f46f54bea4bec9db38
Omar Malki <omarmalki@tlen.pl>
2019-03-01 17:49:49 +0100

    prompt and alert exercise

---
+++ JS/Exercise1JS/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("What's your first name?");
+var userLastName = prompt("What's your last name?");
+var age = prompt("What's your age?");
+alert("Hello " + userFirstName +' '+ userLastName);
+alert("Your age is "+ age +'.'+" Feel old yet?");
