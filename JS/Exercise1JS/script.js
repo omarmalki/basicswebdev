31d3ae0270c0bf87bb6fed0700f684cbc8787ec1
Omar Malki <omarmalki@tlen.pl>
2019-03-05 13:21:02 +0100

    conditional statements exercise

--- JS/Exercise1JS/script.js
+++ JS/Exercise1JS/script.js
@@ -3,3 +3,13 @@ var userLastName = prompt("What's your last name?");
 var age = prompt("What's your age?");
 alert("Hello " + userFirstName +' '+ userLastName);
 alert("Your age is "+ age +'.'+" Feel old yet?");
+
+if( age < 18 ) {
+    alert("You can't enter");
+}
+else if (age >= 18 && age < 21) {
+    alert("You can enter but cannot drink");
+}
+else {
+    alert("you going to drunk as fuck");
+}
