07a0869f9b57b94fc876eef478fb8cd0b2ccc9e9
Omar Malki <omarmalki@tlen.pl>
2019-03-05 14:00:51 +0100

    loops exercise

---
+++ JS/LoopExercise/script.js
@@ -0,0 +1,22 @@
+var number1 = -10;
+while(number1 <= 19) {
+    console.log(number1);
+    number1++;
+}
+
+var number2 = 10;
+while(number2 <= 40) {
+    console.log(number2);
+    number2+=2;
+}
+var number3 = 301;
+while(number3 <= 333) {
+    console.log(number3);
+    number3+=2;
+}
+var number4 = 5;
+while(number4 <= 50) {
+    if(number4 % 5 === 0 && number4 % 3 === 0)
+    console.log(number4);
+    number4++;
+}
