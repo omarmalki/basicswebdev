85ad9b32f9399f89c28be770b3383cd8c42ed15f
Omar Malki <omarmalki@tlen.pl>
2019-03-07 12:15:38 +0100

    functions exercise1

---
+++ JS/functionsExersice/script.js
@@ -0,0 +1,21 @@
+ /*function isEven(number) {
+if(number % 2 === 0){
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+}
+*/
+
+function factorial(num) {
+    if (num === 0){ 
+        return 1; 
+    }
+  else { 
+      return num * factorial( num - 1 ); 
+    }
+}
+function kebabToSnake(str) {
+    console.log(str.replace(/-/g, "_"));
+}
